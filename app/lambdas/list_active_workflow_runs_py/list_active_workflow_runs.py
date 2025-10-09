741b652ae5b2bee3f2ca6a1c7c6a9b9bc697ff4b
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-10-09 14:25:26 +1100

    Fix get active runs
    Handle internal exception for eventbridge, handle legacy workflow manager

--- app/lambdas/list_active_workflow_runs_py/list_active_workflow_runs.py
+++ app/lambdas/list_active_workflow_runs_py/list_active_workflow_runs.py
@@ -8,7 +8,11 @@ Get a list of active workflow runs in the workflow run manager
 from typing import Literal, List, Dict
 
 # Layer imports
-from orcabus_api_tools.workflow import get_workflow_by_workflow_name
+from orcabus_api_tools.workflow import (
+    get_workflow_by_workflow_name,
+    get_workflow_request_response_results
+)
+from orcabus_api_tools.workflow.globals import WORKFLOW_RUN_ENDPOINT
 
 # Globals
 WORKFLOW_NAME = "pieriandx-tso500-ctdna"
@@ -34,6 +38,14 @@ def handler(event, context) -> Dict[str, List[Dict[str, str]]]:
 
     # Get all workflows
     workflows = get_workflow_by_workflow_name(WORKFLOW_NAME)
+
+    if len(workflows) == 0:
+        workflows = get_workflow_request_response_results(
+            WORKFLOW_RUN_ENDPOINT,
+            params={
+                "workflow__workflowName": WORKFLOW_NAME
+            }
+        )
 
     # Get active workflow runs
     active_workflow_runs = list(map(
